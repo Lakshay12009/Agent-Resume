14768a6d5b4dfd084153586990b6de8420a58433
Lakshay12009 <Lakshay.120906@gmail.com>
2026-07-30 10:24:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import numpy as np
 
 
 # to show web-app : complete page layout 
-st.set_page_config(layout="Wide")
+st.set_page_config(layout="wide")
 
 # to give title 
 st.title("AI RESUME GENERATOR ")
